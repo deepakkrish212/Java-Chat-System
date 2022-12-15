7c3cabe0a7879fc00f05a6ccd2b66353eb55e768
abemelech <abemelech.mesfin@gmail.com>
2022-12-15 02:48:26 -0600

    create test driver

---
+++ ChatTest.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ChatTest {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        // Scanner scanner = new Scanner(System.in);
+        String clientName = args[0];
+
+        System.out.println("Enter your name: ");
+        
+        Socket socket = new Socket("localhost", 8080);
+        Client client = new Client(socket, clientName);
+        // These are blocking methods
+        // The client will not be able to send messages until it receives a message from
+        // the server
+        // Since they are sperate threads, the client can send messages and listen for
+        // messages at the same time
+        client.listenForMessage();
+        client.sendMessage();
+    }
+}
---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+javac *.java
+
+start cmd /k java Server
+start cmd /k java ChatTest Abemelech
+start cmd /k java ChatTest Deepak
+
